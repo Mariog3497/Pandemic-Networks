fc57a5fd95d345efdae3f84c18d763871ca7aa45
Mariog3497 <mariog3497@gmail.com>
2025-05-27 09:31:48 +0200

    Main menu

---
+++ main.py
@@ -0,0 +1,19 @@
+
+def main():
+    showMainMenu()
+    
+            
+    
+    
+
+def showMainMenu():
+    print("Network Simulator")
+    print("1. Network Simulation")
+    print("2. Network configuration")
+    print("3. Show pandemic evolution")
+    print("4. Exit")
+    
+
+if __name__ == "__main__":
+
+    main()
